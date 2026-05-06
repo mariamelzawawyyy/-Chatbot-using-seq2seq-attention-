b757aa87f75f46f427ac126d479f17243970ec2a
Mariam Elzawawy <UG_31201767@ics.tanta.edu.eg>
2026-05-07 02:49:07 +0300

    Update GLOVE_PATH to new file location

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ PAD_IDX = 2
 UNK_IDX = 3
 
   
-GLOVE_PATH = r"C:\Users\maria\Downloads\glove.2024.wikigiga.100d\wiki_giga_2024_100_MFT20_vectors_seed_2024_alpha_0.75_eta_0.05.050_combined.txt"
+GLOVE_PATH =  "/content/glove_embeddings /wiki_giga_2024_100_MFT20_vectors_seed_2024_alpha_0.75_eta_0.05.050_combined.txt"
 
 ##############################
 # 2️⃣ Clean Function

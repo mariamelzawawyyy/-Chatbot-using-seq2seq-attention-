551a62db15dc7867ebe23ce94a1904cc36428979
Mariam Elzawawy <UG_31201767@ics.tanta.edu.eg>
2026-05-07 03:10:16 +0300

    Fix GLOVE_PATH variable name and correct path

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ EOS_IDX = 1
 PAD_IDX = 2
 UNK_IDX = 3
 
-  
-GLOVE_PATH = "/content/-Chatbot-using-seq2seq-attention-/glove_embeddings /wiki_giga_2024_100_MFT20_vectors_seed_2024_alpha_0.75_eta_0.05.050_combined.txt"  
+correct_path = "/content/-Chatbot-using-seq2seq-attention-/glove_embeddings/wiki_giga_2024_100_MFT20_vectors_seed_2024_alpha_0.75_eta_0.05.050_combined.txt"
+ 
 
 ##############################
 # 2️⃣ Clean Function
